d72cf33868dff4c03215171d5652d90775ecc173
torresashjian <migueltorreslopez@gmail.com>
2019-02-11 11:31:15 -0800

    Fix removal of images error

--- test/scripts/stop-fab-network.sh
+++ test/scripts/stop-fab-network.sh
@@ -4,4 +4,4 @@
 echo "shutdown fabric test network ..."
 docker kill $(docker ps | egrep 'fabsdkgo|fabric-tools|fabric-ccenv' | awk '{print $1}')
 docker rm $(docker ps -a | egrep 'fabsdkgo|fabric-tools|fabric-ccenv' | awk '{print $1}')
-docker rmi $(docker images | grep fabsdkgo | awk '{print $3}')
+docker images -a | grep "fabsdkgo" | awk '{print $3}' | xargs docker rmi

8c0133dc5a5800b48dec3c2c1a38e5c5bb68d1bc
torresashjian <migueltorreslopez@gmail.com>
2020-07-06 15:38:35 -0700

    Add service

--- cmd/node/start.go
+++ cmd/node/start.go
@@ -1,5 +1,5 @@
 /*
-* Copyright © 2018. TIBCO Software Inc.
+* Copyright © 2020. TIBCO Software Inc.
 * This file is subject to the license terms contained
 * in the license file that is distributed with this file.
  */
@@ -7,8 +7,10 @@
 package node
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 )
 
@@ -23,5 +25,18 @@ var (
 
 // start starts the client service
 func start(cmd *cobra.Command, args []string) {
-	fmt.Println("Client service started")
+	r := mux.NewRouter()
+
+	// GET all releases.
+	r.HandleFunc("/test", TestHandler).Methods("GET")
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":9999", r))
+}
+
+// TestHandler returns a list of all releases for all namespaces
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Success"))
 }
